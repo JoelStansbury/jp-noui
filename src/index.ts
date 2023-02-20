f917f2514cbb836f3807e869d7731dfa96871b7d
Joel Stansbury <Joel.Stansbury@gtri.gatech.edu>
2023-02-20 14:45:20 -0500

    autorun logic, better css

--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,14 @@ import {
 
 import { INotebookTracker, NotebookPanel } from '@jupyterlab/notebook';
 
+var autorun = true;
+var splash_is_visible = false;
 
 const splash_element = document.createElement('div');
 splash_element.classList.add('jp-noui-splash-screen');
-splash_element.innerHTML = "Loading...";
+const loading_anim = document.createElement('div');
+loading_anim.classList.add("ring");
+splash_element.appendChild(loading_anim)
 
 const exit_btn = document.createElement('button')
 exit_btn.classList.add("jp-noui-exit-btn");
@@ -25,6 +29,7 @@ exit_btn.addEventListener('click', (e) => {
   console.log("clicked")
   document.body.removeChild(style);
   document.body.removeChild(exit_btn);
+  autorun = false;
 });
 
 const style = document.createElement('style')
@@ -76,12 +81,13 @@ style.innerHTML = `
 .jp-noui-exit-btn {
   z-index: 999;
   position: absolute;
-  bottom: 0px;
-  left: 0px;
+  bottom: 1px;
+  left: 1px;
   background: white;
   border: none;
   font-family: system-ui;
 }
+
 .jp-noui-splash-screen {
   z-index: 1000;
   position: absolute;
@@ -90,11 +96,46 @@ style.innerHTML = `
   width: 100%;
   font-family: system-ui;
 }
+
+.ring
+{
+  position:absolute;
+  top:50%;
+  left:50%;
+  transform:translate(-50%,-50%);
+  width:150px;
+  height:150px;
+  background:transparent;
+  border:8px solid #ffffff;
+  border-radius:50%;
+}
+.ring:before
+{
+  content:'';
+  position:absolute;
+  top:-8px;
+  left:-8px;
+  width:100%;
+  height:100%;
+  border:8px solid transparent;
+  border-top:8px solid #ff8539;
+  border-right:8px solid #ff8539;
+  border-radius:50%;
+  animation:animate 1.2s linear infinite;
+}
+@keyframes animate
+{
+  0%
+  {
+    transform:rotate(0deg);
+  }
+  100%
+  {
+    transform:rotate(360deg);
+  }
+}
 `
 
-/**
- * A splash screen for jp-noui
- */
 const splash: JupyterFrontEndPlugin<ISplashScreen> = {
   id: '@jp-noui/jp-noui:plugin',
   autoStart: true,
@@ -107,14 +148,19 @@ const splash: JupyterFrontEndPlugin<ISplashScreen> = {
 
     document.body.appendChild(style);  // Hide jlab garbage
     document.body.appendChild(splash_element);  // Show splash screen
+    splash_is_visible = true;
     document.body.appendChild(exit_btn);  // Show button to exit
+
 
     // Add listener to NotebookTracker
     tracker.currentChanged.connect((_: INotebookTracker, nbp: NotebookPanel | null) => {
-      if (nbp) {
+      if (nbp && autorun) {
         nbp.sessionContext.ready.then(() => {
           app.commands.execute("notebook:run-all-cells");
-          document.body.removeChild(splash_element);
+          if (splash_is_visible) {
+            document.body.removeChild(splash_element);
+            splash_is_visible = false;
+          }
         });
       }
     });
